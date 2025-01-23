2c2a273cb671a46dd20b0ac6c5a09bf982de0cd4
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-01-23 13:57:59 +0000

    Add personal details to the web page

--- src/main.js
+++ src/main.js
@@ -4,15 +4,20 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
-    // In the following fiels you can either give a single string,
+    name: "Alistair White-Horne",
+    // In the following fields you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: [
+        "Awesome things",
+        "Testing",
+        "Building",
+        "Deploying"
+    ],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "None at all - never used any before...",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: "Everythinggg!",
   },
 });
